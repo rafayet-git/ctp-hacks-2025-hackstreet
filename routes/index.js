a3d9be0495c3e3262bdf9a848c1dd55ddfa9b4a6
Camilo Mason <camilomason0@gmail.com>
2025-08-20 11:24:29 -0400

    Basic route setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Outfit Tracker' });
+});
+router.get('/outfits/new', function(req, res, next) {
+  res.render('outfits/new', { title: 'New Outfit' });
+});
+router.get('/outfits', function(req, res, next) {
+  res.render('outfits/index', { title: 'Outfits' });
 });
 
 module.exports = router;
