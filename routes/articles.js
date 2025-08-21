968baf38124713d52c814c5faa37717bd1555851
Camilo Mason <camilomason0@gmail.com>
2025-08-21 11:50:32 -0400

    Add basic views for articles

--- routes/articles.js
+++ routes/articles.js
@@ -2,10 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET articles listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) { // INDEX
   res.render('articles/index', {
-    title: 'Articles',
+    title: 'Articles'
   });
+});
+router.get('/new', (req, res, next) => {
+  res.render('articles/new', {
+    title: 'Create article'
+  });
+});
+router.post('/new', (req, res, next) => {
+  // TODO: validate data
+  // TODO: save data in DB
+  res.redirect('articles/1');
 });
 
 module.exports = router;
--- routes/outfits.js
+++ routes/outfits.js
@@ -1,26 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 /* GET outfits listing. */
-router.get('/new', (req, res, next) => {
-  res.render('outfits/new', { 
+router.get('/new', (req, res, next) => { // NEW (make sure this route comes BEFORE the SHOW route in this file)
+  res.render('outfits/new', {
     title: 'New Outfit',
   });
 });
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => { // SHOW
   // ~~~logic for using :id to get outfit from DBMS goes here~~~
-  res.render('outfits/show', { 
+  
+  res.render('outfits/show', {
     outfit: { title: "Test" },
     title: 'Outfit Details'
   });
 });
-router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => { // INDEX
   // TODO: get all outfits for a specific user from DBMS
-  res.render('outfits/index', { 
+  res.render('outfits/index', {
     title: 'Outfits'
   });
 });
-router.post('/new', (req, res, next) => {
+router.post('/new', (req, res, next) => { // CREATE
   // TODO: validate data and insert new outfit into DB
   res.redirect(`/outfits/1`) // TODO: replace 1 with id of newly created outfit
 });
