3c3b6dff7041742d6ab6c0f6a3c7534c87c0e1f9
Camilo Mason <camilomason0@gmail.com>
2025-08-21 13:21:41 -0400

    Add db logic to outfits for new view

--- routes/outfits.js
+++ routes/outfits.js
@@ -3,24 +3,33 @@ var router = express.Router();
 const db = require('../db');
 
 /* GET outfits listing. */
-router.get('/new', (req, res, next) => { // NEW (make sure this route comes BEFORE the SHOW route in this file)
-  res.render('outfits/new', {
-    title: 'New Outfit',
-  });
-});
-router.get('/:id', (req, res) => { // SHOW
-  // ~~~logic for using :id to get outfit from DBMS goes here~~~
-  
-  res.render('outfits/show', {
-    outfit: { title: "Test" },
-    title: 'Outfit Details'
-  });
-});
 router.get('/', (req, res, next) => { // INDEX
   // TODO: get all outfits for a specific user from DBMS
   res.render('outfits/index', {
     title: 'Outfits'
   });
+});
+router.get('/new', (req, res, next) => { // NEW (make sure this route comes BEFORE the SHOW route in this file)
+  let query = `SELECT * FROM article`;
+  db.query(query, (err, result, fields) => {
+    if (err) throw err;
+    res.render('outfits/new', {
+      title: 'New Outfit',
+      articles: result
+    });
+  });
+});
+router.get('/:id', (req, res) => { // SHOW
+  // ~~~logic for using :id to get outfit from DBMS goes here~~~
+  let query = `SELECT * FROM outfit WHERE id=${req.params.id}`;
+  db.query(query, (err, result, fields) => {
+    if (err) throw err;
+    res.render('outfits/show', {
+      title: 'Outfit Details',
+      outfit: result[0]
+    });
+  });
+  
 });
 router.post('/new', (req, res, next) => { // CREATE
   // TODO: validate data and insert new outfit into DB
